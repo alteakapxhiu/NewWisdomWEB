9cd7033c6903e6372547bc6f65f832e1909d53eb
Altea <alteakapxhiu@gmail.com>
2025-08-27 16:12:12 +0200

    Backend Dev

--- cloudinary-server/server.js
+++ cloudinary-server/server.js
@@ -13,7 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://127.0.0.1:5500',
+        "https://newisdomcenter.vercel.app"
+    ]
+}));
+
 const upload = multer({ storage: multer.memoryStorage() });
 
 cloudinary.config({
@@ -85,5 +91,5 @@ app.post('/submit-contact', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
